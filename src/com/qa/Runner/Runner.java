f253d7fac224b9b15f9e24b449c5ad28f7b9b5c3
Ranjan <ranjan.bit17@gmail.com>
2020-10-21 16:31:33 +0530

    Runner

---
+++ src/com/qa/Runner/Runner.java
@@ -0,0 +1,20 @@
+package com.qa.Runner;
+
+import java.util.List;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		  List<String> suites = Lists.newArrayList();         
+	       TestListenerAdapter tla = new TestListenerAdapter();
+	       TestNG testng = new TestNG();
+	       suites.add(System.getProperty("user.dir")+"//testng.xml");
+	       testng.setTestSuites(suites);
+	       testng.addListener(tla);
+	       testng.run();
+	}
+
+}
