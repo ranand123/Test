0993df4769b3a1f80b5d078acc31155070eb96dc
Ranjan <ranjan.bit17@gmail.com>
2020-10-21 16:29:21 +0530

    firat

--- src/com/qa/JenkinsTest/LoginTest.java
+++ src/com/qa/JenkinsTest/LoginTest.java
@@ -9,6 +9,9 @@ public class LoginTest {
 	
 	WebDriver driver;
 	
+	int a;
+	String test="123";
+	
 	@BeforeTest
 	public void initObj() {
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
@@ -18,6 +21,7 @@ public class LoginTest {
 	@Test
 	public void testLogin() {
 		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
 		System.out.println("Google Launch successfully");
 	}
 
