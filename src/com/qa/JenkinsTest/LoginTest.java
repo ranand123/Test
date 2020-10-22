6c21cd450f0a2bef4622e5f1bbb567ad9beb0aed
ranand123 <40069494+ranand123@users.noreply.github.com>
2020-10-22 23:52:23 +0530

    Update LoginTest.java

--- src/com/qa/JenkinsTest/LoginTest.java
+++ src/com/qa/JenkinsTest/LoginTest.java
@@ -2,18 +2,28 @@ package com.qa.JenkinsTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class LoginTest {
 	
 	WebDriver driver;
 	
-	int a;
-	String test="123";
+	static ExtentTest test;
+	static ExtentReports report;
 	
 	
-	
+	@BeforeClass
+	public static void startTest()
+	{
+	report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html");
+	test = report.startTest("ExtentDemo");
+	}
 	
 	@BeforeTest
 	public void initObj() {
@@ -26,6 +36,9 @@ public class LoginTest {
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		System.out.println("Google Launch successfully");
+		test.log(LogStatus.PASS, "Navigated to the specified URL");
+		report.endTest(test);
+		report.flush();
 	}
 
 }
