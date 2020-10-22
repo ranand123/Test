34524ae3ea9973afe493c0cf284f83ea28644883
Ranjan <ranjan.bit17@gmail.com>
2020-10-22 16:01:23 +0530

    testcommit

--- src/com/qa/Runner/Runner.java
+++ src/com/qa/Runner/Runner.java
@@ -1,6 +1,9 @@
 package com.qa.Runner;
 
 import java.util.List;
+
+import javax.swing.plaf.synth.SynthStyle;
+
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 import org.testng.collections.Lists;
@@ -10,6 +13,8 @@ public class Runner {
 	public static void main(String[] args)  {
 		  List<String> suites = Lists.newArrayList();
 		
+		  System.out.println("Test message");
+		  
 	       TestListenerAdapter tla = new TestListenerAdapter();
 	       TestNG testng = new TestNG();
 	       suites.add(System.getProperty("user.dir")+"//testng.xml");
